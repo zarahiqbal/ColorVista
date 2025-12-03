24d847cd0938de69a20b8d7e320ab505dea03231
Mudassir-ali228 <amudassir4321@gmail.com>
2025-12-03 15:15:53 +0500

    commented code deleted

--- app/difficulty.tsx
+++ app/difficulty.tsx
@@ -1,38 +1,3 @@
-// import { DifficultySelection } from '../screens/DifficultyLevel';
-
-// export default function DifficultyScreen() {
-//   return (
-//     <DifficultySelection
-//       onSelectDifficulty={(difficulty) => {
-//         console.log('Selected difficulty:', difficulty);
-//         // later you can route to test screen:
-//         // router.push(`/test?level=${difficulty}`);
-//       }}
-//     />
-//   );
-// }
-
-// import { useRouter } from "expo-router";
-// import { DifficultySelection } from "../screens/DifficultyLevel";
-
-// export default function DifficultyScreen() {
-//   const router = useRouter();
-
-//   const handleSelect = (difficulty: "easy" | "hard") => {
-//     router.push({
-//       pathname: "/quiz",
-//       params: { difficulty },
-//     });
-//   };
-
-//   return <DifficultySelection onSelectDifficulty={handleSelect} />;
-// }
-
-
-
-
-
-
 import { useRouter } from "expo-router";
 import { DifficultySelection } from "../screens/DifficultyLevel";
 
@@ -48,17 +13,3 @@ export default function DifficultyScreen() {
 
   return <DifficultySelection onSelectDifficulty={handleSelect} />;
 }
-
-//upp one right
-// import DifficultyLevel from "../screens/DifficultyLevel";
-
-// export default function DifficultyScreen() {
-//   return (
-//     <DifficultyLevel
-//       onSelectDifficulty={(level) => {
-//         console.log("Selected:", level);
-//         // router.push(`/quiz?mode=${level}`)
-//       }}
-//     />
-//   );
-// }
