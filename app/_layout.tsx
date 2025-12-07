06899a1742d2210f1d3e0c61c4261c5ad9b8d980
zarah <zarahiiqbal@gmail.com>
2025-12-07 07:53:02 +0500

    Removed all extrs comments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,3 @@
-// import { Stack } from "expo-router";
-
-// export default function Layout() {
-//   return (
-//     <Stack>
-//        <Stack.Screen
-//         name="dashboard"
-//         options={{
-//           headerShown: false, // This removes the "dashboard" text and white space
-//         }}
-//       />
-//       <Stack.Screen
-//         name="welcome"
-//         options={{
-//           title: "Welcome",
-//           headerShown: false,
-//         }}
-//       />
-//       <Stack.Screen
-//         name="difficulty"
-//         options={{ headerShown: false }}
-//       />
-//       <Stack.Screen
-//         name="mediaupload"
-//         options={{
-//           headerShown: false, // This removes the "dashboard" text and white space
-//         }}
-//       />
-//       <Stack.Screen
-//         name="live"
-//         options={{
-//           headerShown: false, // This removes the "dashboard" text and white space
-//         }}
-//       />
-//       <Stack.Screen
-//         name="quiz"
-//         options={{
-//           headerShown: false, // This removes the "dashboard" text and white space
-//         }}
-//       />
-//     </Stack>
-//   );
-// }
-
-
 import { Stack } from 'expo-router';
 // Make sure this path points to where you moved the file
 import { AuthProvider } from '@/Context/AuthContext';
@@ -68,18 +23,3 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
-// import { Stack } from 'expo-router';
-
-// export default function RootLayout() {
-//   return (
-//     <Stack screenOptions={{ headerShown: false }}>
-//       {/* Expo Router automatically detects screens based on files.
-//         We just configure the stack behavior here.
-//       */}
-//       <Stack.Screen name="index" />
-//       <Stack.Screen name="auth/login/index" />
-//       <Stack.Screen name="auth/signup/index" />
-//       <Stack.Screen name="dashboard/index" />
-//     </Stack>
-//   );
-// }
